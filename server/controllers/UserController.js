7dd5c37f5e5cbef37da2b243f1a5e594b8b342c1
amgeraldo14 <amgeraldo14@gmail.com>
2021-02-05 10:05:59 +0700

    google login, logout, ama api doc dikit

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,7 @@
 const { User } = require('../models/index')
 const { comparePassword } = require('../helpers/bcrypt')
 const { generateToken } = require('../helpers/jwt')
+const {OAuth2Client} = require('google-auth-library');
 
 class UserController{
   static register(req, res, next){
@@ -40,11 +41,58 @@ class UserController{
         email: user.email
       }
       let access_token = generateToken(payload)
-      res.status(200).json(access_token)
+      res.status(200).json({access_token})
 
     } catch (err) {
       next(err)
     }
+  }
+
+  static googleLogin(req, res, next){
+    let {id_token} = req.body
+    // console.log(id_token, 'ini buat google login')
+    const client = new OAuth2Client(process.env.CLIENT_ID)
+    let email = ''
+    let password = ''
+    // console.log(client, '<<<< ini client')
+    client.verifyIdToken({
+      idToken: id_token,
+      audience: process.env.CLIENT_ID
+    })
+    .then(ticket => {
+      const payload = ticket.getPayload()
+      email = payload.email
+      password = 'randomPassword'
+      //findOne by email dulu buat cek emailnya udah ada atau belum. kalo blm ada, dibikin dulu. kalo udah ada, lanjut login
+      return User.findOne({
+        where: {email}
+      })
+    })
+    .then(user => {
+      if (user){
+        // console.log('masuk if then kedua')
+        return user
+      } else{
+        // console.log('masuk else then kedua')
+        return User.create({
+          email,
+          password
+        })
+      }
+    })
+    .then(user => {
+      const {id, email} = user
+      const payload = {
+        id,
+        email
+      }
+      const access_token = generateToken(payload)
+      // console.log(access_token, 'ini access token')
+      res.status(200).json(access_token)
+    })
+    .catch(err => {
+      res.status(500).json({msg: 'internal server error'})
+    })
   }
 }
 
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,6 +4,7 @@ const router = require('express').Router()
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.post('/login/google', UserController.googleLogin)
 
 
 module.exports = router
